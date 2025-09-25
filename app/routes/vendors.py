d3323ab780321369c402afa1ded0009977bab6f0
techkoder <vinaykjain333@gmail.com>
2025-09-26 01:15:47 +0530

    added delete functionality for vendors

--- app/routes/vendors.py
+++ app/routes/vendors.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, render_template, request, jsonify,flash
 from app.models.vendor import Vendor
 
 vendors_bp = Blueprint('vendors', __name__, url_prefix='/vendors')
@@ -8,13 +8,13 @@ def list_vendors():
     """List all vendors"""
     try:
         vendors = Vendor.get_all()
-        return render_template('vendors/list.html', vendors=vendors,add_url="/vendors/addForm",view_url="/vendors")
+        return render_template('vendors/list.html', vendors=vendors,add_url="/vendors/addForm",view_url="/vendors",delete_url="/vendors/DeleteForm")
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 @vendors_bp.route('/addForm')
 def add_vendorsForm():
-    return render_template('forms/vendor_form.html',add_url="/vendors/addForm",view_url="/vendors")
+    return render_template('forms/vendor_form.html',add_url="/vendors/addForm",view_url="/vendors",delete_url="/vendors/DeleteForm")
 
 @vendors_bp.route('/add',methods=['POST'])
 def add_vendors():
@@ -22,4 +22,14 @@ def add_vendors():
     print(vendor_data)
     Vendor.create(vendor_data)
     vendors = Vendor.get_all()
-    return render_template('vendors/list.html',vendors = vendors,add_url="/vendors/addForm",view_url="/vendors")
+    return render_template('vendors/list.html',vendors = vendors,add_url="/vendors/addForm",view_url="/vendors",delete_url="/vendors/DeleteForm")
+
+@vendors_bp.route('/DeleteForm')
+def deleteVendorsForm():
+    return render_template('vendors/delete.html',add_url="/vendors/addForm",view_url="/vendors",delete_url="/vendors/DeleteForm")
+
+@vendors_bp.route('/delete',methods=['POST'])
+def deleteVendor():
+    ven_code = request.values.get('ven_code')
+    Vendor.delete(ven_code)
+    return render_template('vendors/delete.html',add_url="/vendors/addForm",view_url="/vendors",delete_url="/vendors/DeleteForm")
--- run.py
+++ run.py
@@ -1,45 +1,6 @@
-# import flask
-
-# app = flask.Flask(__name__)
 from app import models
 import os
-# from flask import render_template
 app = models.create_app(os.environ.get('FLASK_CONFIG') or 'default')
-
-# @app.route('/sales')
-# def sales_orders():
-#     return render_template('forms/sales_order_form.html')
-# @app.route('/purchase')
-# def purchase_orders():
-#     return render_template('forms/purchase_order_form.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
-# @app.route('/')
-# def dashboard():
-#     return render_template('dashboard.html')
-
-# @app.route('/items')
-# def items():
-#     return render_template('forms/item_form.html')
-
-# @app.route('/items_list.html')
-# def items_list():
-#     return render_template('items/list.html')
-
-# @app.route('/vendors')
-# def vendors():
-#     return render_template('forms/vendor_form.html')
-
-# @app.route('/customers')
-# def customers():
-#     return render_template('forms/customer_form.html')
-
-
-
-
-# @app.route('/reports')
-# def reports():
-#     return render_template('reports/report.html')
-
-# app.run(debug=True)
